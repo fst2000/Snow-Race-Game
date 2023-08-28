f08f8d9f69fc91d9cec794748cddbd92745a8672
fst2000 <fst.18.work@gmail.com>
2023-08-28 15:10:28 +0400

    Collision based ski sliding

--- Assets/Source/SnowcatBehaviour.cs
+++ Assets/Source/SnowcatBehaviour.cs
@@ -1,25 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SnowcatBehaviour : MonoBehaviour
 {
-    RayHit hit;
+    [SerializeField] Transform handlingSki;
+    [SerializeField] float slideForce;
     Event fixedUpdate;
+    new Rigidbody rigidbody;
+    List<ContactPoint> contacts;
+    bool hasCollision;
     void Start()
     {
         fixedUpdate = new Event();
-
-        Vector3Func down = v3Action => v3Action(-transform.up);
-        Vector3Func position = v3Action => v3Action(transform.position);
-        hit = new RayHit(fixedUpdate, position, down);
+        rigidbody = gameObject.GetComponent<Rigidbody>();
+        contacts = new();
     }
     void Update()
     {
-        hit.Accept(hit => Debug.Log(hit.distance));
+        handlingSki.rotation = Quaternion.LookRotation(transform.forward, transform.up) * Quaternion.Euler(0, Input.GetAxis("Horizontal") * 60, 0);
+    }
+    void OnCollisionStay(Collision other)
+    {
+        other.GetContacts(contacts);
+        hasCollision = true;
     }
     void FixedUpdate()
     {
         fixedUpdate.Call();
+        if(hasCollision)
+        {
+            foreach(var contact in contacts)
+            {
+                Vector3 frictionDirection = Vector3.Cross(contact.thisCollider.transform.forward, contact.normal);
+                float frictionScalar = Vector3.Dot(rigidbody.transform.right, rigidbody.velocity);
+                Debug.DrawRay(contact.point, frictionDirection);
+                rigidbody.AddForceAtPosition(frictionDirection * frictionScalar * slideForce, contact.point);
+            }
+        }
+        hasCollision = false;
     }
 }
